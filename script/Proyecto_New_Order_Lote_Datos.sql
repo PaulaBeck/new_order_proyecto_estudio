0d296bef28cbbf626dd66e089c1c73f0ee4fba0a
Ana <beck.anapau@gmail.com>
2024-11-07 17:29:38 -0300

    Actualizar archivos y agregar carpetas

---
+++ script/Proyecto_New_Order_Lote_Datos.sql
@@ -0,0 +1,10 @@
+/*	BASE DE DATOS I - 2024
+	PROYECTO DE ESTUDIO - NEW ORDER
+	COMISION 2 - GRUPO 3 
+*/
+
+-----------------------------------
+-- LOTE DE DATOS
+-----------------------------------
+
+USE DB_NEW_ORDER;	-- Ubicarse en la BD a trabajar
